a25323fe90c7792dccecd7b4813ef8bdb0b1d49e
Yalçın <kovancilartr@gmail.com>
2024-12-21 23:46:31 +0300

    build alındı8

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello World!");
 });
 app.use("/api/v1", customerRouter);
